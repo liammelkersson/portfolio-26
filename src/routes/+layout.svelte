0a707c09a802dd86b1794859ba34414eee0b4364
liammelkersson <liammelkersson@live.se>
2026-07-04 11:02:22 +0200

    Use circular portrait as favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import AsciiBackground from '$lib/components/AsciiBackground.svelte';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href="/favicon.png" type="image/png" />
+	<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 </svelte:head>
 
 <div class="min-h-screen font-sans text-black antialiased dark:text-white">
