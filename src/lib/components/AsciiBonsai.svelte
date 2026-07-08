dc26fa2158dde40038eee799d6ce5a6ab829cc73
liammelkersson <liammelkersson@live.se>
2026-07-08 14:26:43 +0200

    Replace bonsai art with cleaner tree-in-pot design, left-aligned

--- src/lib/components/AsciiBonsai.svelte
+++ src/lib/components/AsciiBonsai.svelte
@@ -1,14 +1,14 @@
-<pre class="ascii-bonsai mx-auto w-fit font-mono text-sm leading-tight opacity-60" aria-hidden="true">
-        ,~~,
-       (    )
-        `--'
-    ,~~,
-   (    )---.
-    `--'      \
-           ,~~,
-          (    )
-           `--'
-            |
-        ____|____
-       (_________)
+<pre class="ascii-bonsai w-fit font-mono text-sm leading-tight opacity-60" aria-hidden="true">
+     ,\
+    # (_
+      _)\##
+  ###/((_
+       ))\####
+     _((
+####/  )\
+     ,;;"`;,
+    (_______)
+      \===/
+      /===\
+     /=====\
 </pre>
